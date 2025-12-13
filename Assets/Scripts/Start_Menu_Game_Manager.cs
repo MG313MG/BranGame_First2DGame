a6b8a75551719210004750aa566ee24af2492fcc
Mostafa <mg313mg313mg@gmail.com>
2025-12-13 22:10:13 +0330

    Start_menu Created

---
+++ Assets/Scripts/Start_Menu_Game_Manager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Start_Menu_Game_Manager : MonoBehaviour
+{
+    public GameObject Start_menu;
+    public GameObject Play_List;
+
+    public void Open_Play_List()
+    {
+
+    }
+}
